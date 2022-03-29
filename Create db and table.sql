dd49caf35c069cea9d9791b973925a8c9a5158d1
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2022-03-29 08:46:47 +0200

    Add readme and database script

--- Create db and table.sql
+++ Create db and table.sql
@@ -2,22 +2,22 @@ USE [master]
 GO
 
 /****** Object:  Database [Casus2021B3]    Script Date: 23-3-2021 08:40:42 ******/
-CREATE DATABASE [Casus2021B3]
+CREATE DATABASE [SensorData]
 GO
 
-USE [Casus2021B3]
+USE [SensorData]
 GO
 
-/****** Object:  Table [dbo].[LoRaSensorReading]    Script Date: 23-3-2021 08:42:26 ******/
+/****** Object:  Table [dbo].[SensorReading]    Script Date: 23-3-2021 08:42:26 ******/
 
 
-CREATE TABLE [dbo].[LoRaSensorReading](
+CREATE TABLE [dbo].[SensorReading](
 	[EntryId] [bigint] IDENTITY(1,1) NOT NULL,
 	[SensorId] [varchar](1) NULL,
-	[Car_in] [bigint] NULL,
-	[Car_out] [bigint] NULL,
+	[People_in] [bigint] NULL,
+	[People_out] [bigint] NULL,
 	[TimeStamp] [varchar](12) NULL,
- CONSTRAINT [PK_LoRaSensorData] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_SensorData] PRIMARY KEY CLUSTERED 
 (
 	[EntryId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-SensorDatagenerator
+#SensorDatagenerator
+Deze oplossing kun je gebruiken om sensordata te genereren. Bekijk goed hoe de code werkt en gebruik hem om data te genereren in de database of kopieer gewenste stukken van de code naar je eigen oplossing.
+Op alle plekken in de oplossing is commentaar in de code geplaatst wat zou moeten helpen beter te begrijpen wat er gebeurt.
+
+
+#Prerequisites
+-Visual Studio met .net6.0
+-Toegankelijke SQL database server
+
+#Stappenplan
+
+-Maak een database aan genaamd "Sensordata"
+-Voer het script [Create db and table.sql](Create db and table.sql) uit
+-Build en run de code
