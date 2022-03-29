2da65a3c5f267e7c0087558f272da1d8c8974894
Rob Cilissen <rob.cilissen-grassere@zuyd.nl>
2022-03-29 09:56:49 +0200

    Optimize console output

--- Program.cs
+++ Program.cs
@@ -8,32 +8,36 @@ namespace SensorDataGenerator
     {
         static void Main(string[] args)
         {
+            // get connection info
             Console.WriteLine("DATABASE CONNECTION:");
             Console.WriteLine("Enter database servername (default: .)");
             var server = Console.ReadLine();
             Console.WriteLine("Enter catalog name (default: SensorData)");
             var db = Console.ReadLine();
 
-            Console.Clear();
 
+            // get location and sensor info
+            Console.Clear();
             Console.WriteLine("Creating location placeholder!");
-            Console.WriteLine("Enter number of days for data generation (default 60)");
-            
-            var daysStr = Console.ReadLine();
-            int daysInt;
-            try
-            {
-                daysInt = Int32.Parse(daysStr);
-            }
-            catch (Exception)
-            {
-                daysInt = 60;
-            }
+
+            int daysInt = GetNumerFromConsole("Enter number of days for data generation", 60);
             daysInt = -1 * daysInt;
+
+            int numberOfSensors = GetNumerFromConsole("Enter the number of sensors, aka the numbers om entrances of the location", 1);
+            int maxPeopleLocation = GetNumerFromConsole("Enter the maximum number of people in the location", 200);
             
             DAL LocationDAL;
             // Create the dal for the given connection. Provide the number of days to calculate from, the number of sensors (number of entries of the location) and the max number of persons in the location
-            LocationDAL = new DAL(DateTime.Now.Add(new TimeSpan(daysInt, 0, 0, 0)), server, db, 1, 200);
+            LocationDAL = new DAL(DateTime.Now.Add(new TimeSpan(daysInt, 0, 0, 0)), server, db, numberOfSensors, maxPeopleLocation);
+
+            // summary
+            Console.Clear();
+            Console.WriteLine($"Database server {server}");
+            Console.WriteLine($"Database catalog {db}");
+            Console.WriteLine($"Number of days to start generating data {daysInt}");
+            Console.WriteLine($"Number of sensors {numberOfSensors}");
+            Console.WriteLine($"Max number of persons in the location {maxPeopleLocation}");
+            Console.WriteLine();
 
             // check connection
             Console.WriteLine(LocationDAL.CheckDatabaseConnection());
@@ -47,6 +51,28 @@ namespace SensorDataGenerator
             // check connection (show number of records)
             Console.WriteLine(LocationDAL.CheckDatabaseConnection());
 
+        }
+
+        /// <summary>
+        /// Get an integer value from console input
+        /// </summary>
+        /// <param name="_text">The text to show</param>
+        /// <param name="_default">The default value for the int in case an invalid value is entered</param>
+        /// <returns></returns>
+        public static int GetNumerFromConsole(string _text, int _default)
+        {
+            Console.WriteLine($"{_text} (Default {_default})");
+            var readlineValue = Console.ReadLine();
+            int theValueInt;
+            try
+            {
+                theValueInt = Int32.Parse(readlineValue);
+            }
+            catch (Exception)
+            {
+                theValueInt = _default;
+            }
+            return theValueInt;
         }
     }
 }
